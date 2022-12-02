43022944c7b07a686fab098005a2e94addcd4fc4
Giacomo D'angelo <118721242+jackydangelo@users.noreply.github.com>
2022-12-02 15:38:59 +0100

    Update Extra1MappingValue.vb

--- Extra1MappingValue.vb
+++ Extra1MappingValue.vb
@@ -2,15 +2,15 @@ Sub MappingValue()
 Dim i As Integer
 
 i = 2
-Do While Not IsEmpty(Worksheets("Account").Cells(i, 4))
-Select Case Worksheets("Account").Cells(i, 4).Value
+Do While Not IsEmpty(Worksheets("Sheet1").Cells(i, 4))
+Select Case Worksheets("Sheet1").Cells(i, 4).Value
 
 Case "Libero"
-Worksheets("Account").Cells(i, 4) = "L"
+Worksheets("Sheet1").Cells(i, 4) = "L"
 Case "Salvaguardia"
-Worksheets("Account").Cells(i, 4) = "S"
+Worksheets("Sheet1").Cells(i, 4) = "S"
 Case "Tutela"
-Worksheets("Account").Cells(i, 4) = "T"
+Worksheets("Sheet1").Cells(i, 4) = "T"
 
 End Select
 i = i + 1
